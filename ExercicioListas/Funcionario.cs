a5cafe5aa818fe6b0b33005748f9361c8c2ab477
Hiury Gomes <hiury_jp@hotmail.com>
2021-10-01 16:29:53 -0300

    Exercicio Listas

---
+++ ExercicioListas/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace ExercicioListas
+{
+    class Funcionario
+    {
+        public int Id { get; conjunto; }
+        nome da corda pública { obter; conjunto; }
+        salário duplo público { obter;  conjunto privado;
+    }
+
+    Funcionáriopúblico(int id, nome da corda , salário duplo)
+    {
+        Id = id;
+        Nome = nome;
+        Salário = salário;
+    }
+
+    vazio público Aumentosalary(percentual duplo)
+    {
+        Salário += Salário * percentual / 100. 0;
+    }
+
+    público substituir corda ToString()
+    {
+        ID de retorno
+            + ", disse.
+            + Nome
+            + ", disse.
+            + Salário.ToString("F2", CultureInfo.Cultura Invariante);
+    }
+}
+
